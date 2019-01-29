8893882a0358ac0358f8e86978693bcffa8b958c
kota-kunioka <kunioka.kouta@gmail.com>
2019-01-29 16:29:08 +0900

    test

--- window.py
+++ window.py
@@ -1,23 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import pygame
-from pygame.locals import *
-import sys
- 
-SCREEN_SIZE = (640, 480)  # 画面サイズdayo
+# ブロック崩し
+from tkinter import *
 
-# Pygameを初期化
-pygame.init()
-# SCREEN_SIZEの画面を作成
-screen = pygame.display.set_mode(SCREEN_SIZE)
-# タイトルバーの文字列をセット
-pygame.display.set_caption(u"ウィンドウの作成")
- 
+# ウィンドウの作成
+win = Tk()
+win.title("window")
+cv = Canvas(win, width = 600, height = 400)
+cv.pack()
+
 # ゲームループ
-while True:
-    screen.fill((0,0,255))   # 画面を青色で塗りつぶす
-    pygame.display.update()  # 画面を更新
-    # イベント処理
-    for event in pygame.event.get():
-        if event.type == QUIT:  # 終了イベント
-            sys.exit()
+def game_loop():
+    win.after(50, game_loop)
+
+game_loop()
+win.mainloop() # ゲームウィンドウを表示
